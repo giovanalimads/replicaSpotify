4f9240ee6939dd0d68d9acfa653e9c39eef0c53a
giovanalimads <gilimaolliveira@gmail.com>
2025-02-16 20:45:26 -0300

    commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,12 +5,434 @@ import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 const Main = () => {
   return (
     <div className="main">
-        <div className="main__texts">
-            <h2>Artistas populares</h2>
-            <a className="main__link" href="/">Mostrar tudo</a>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar tudo
+          </a>
         </div>
-    </div>
-  )
-}
 
-export default Main
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Músicas populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar tudo
+          </a>
+        </div>
+
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4"
+                  alt="Imagem do Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon "
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Amo Noite E Dia - Live In Sao Paulo / 2010
+                </p>
+              </div>
+
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
