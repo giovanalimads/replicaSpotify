cbe96c4d818dd97ff24e063ee1abfb51a5f0aedb
giovanalimads <gilimaolliveira@gmail.com>
2025-02-16 19:04:42 -0300

    Primeiro commit

---
+++ src/App2.jsx
@@ -0,0 +1,35 @@
+// rafce
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App2
+
+
+// Arrow function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de componente
+// PascalCase
+
+// Nomeação de variável, função
+// camelCase
+
+// export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com o nome exportado
+
+// self closing tag
+// <Header></Header> ou <Header />
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks, Elements, Modifiers
+// bloco__elemento--modificador => header__link--small
+
+// Tag vazia em React se chama Fragment ou Fragmento
